3fa8a32d7de77c121d68b64a24616ffa04ca5826
DevNeedHelp <116783534+DevNeedHelp@users.noreply.github.com>
2022-11-07 14:15:35 +0700

    Update Calculator but bahasa and English Mixed.py

--- Calculator but bahasa and English Mixed.py
+++ Calculator but bahasa and English Mixed.py
@@ -11,13 +11,13 @@ bilangan_1 = float(input('Enter first number:'))
 bilangan_2 = float(input('Enter second Number:'))
 print('='*20)
 
-if calculator ='1':
+if calculator =='1':
     print('user choosed Jumlah')
-elif calculator ='2':
+elif calculator =='2':
     print('user choosed Kurang')
-elif calculator ='3':
+elif calculator =='3':
     print('user choosed Pembagian')
-elif calculator ='4':
+elif calculator =='4':
     print('user choosed Perkalian')
 
 if calculator =='1':
