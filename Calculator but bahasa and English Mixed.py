91937726ac50d06d1b28dda0253fde536e6e3c99
DevNeedHelp <116783534+DevNeedHelp@users.noreply.github.com>
2022-11-07 14:07:22 +0700

    Create Calculator but bahasa and English Mixed.py
    for fun :v

---
+++ Calculator but bahasa and English Mixed.py
@@ -0,0 +1,42 @@
+print('Calculator Inggris campur Indo')
+print('='*20)
+print(' 1. Jumlah\t [+]')
+print(' 2. Kurang\t [-]')
+print(' 3. Pembagian\t [/]')
+print(' 4. Perkalian\t [+]')
+print('='*20)
+calculator = input('pilih operasi 1/2/3/4:')
+print('='*20)
+bilangan_1 = float(input('Enter first number:'))
+bilangan_2 = float(input('Enter second Number:'))
+print('='*20)
+
+if calculator ='1':
+    print('user choosed Jumlah')
+elif calculator ='2':
+    print('user choosed Kurang')
+elif calculator ='3':
+    print('user choosed Pembagian')
+elif calculator ='4':
+    print('user choosed Perkalian')
+
+if calculator =='1':
+    hasil= bilangan_1+bilangan_2
+    print("Result:",(bilangan_1+bilangan_2))
+
+elif calulator =='2':
+    hasil = bilangan_1-bilangan_2
+    print("Result:",(bilangan_1-bilangan_2))
+
+elif calculator =='3':
+    hasil = bilangan_1/bilangan_2
+    print("Result:",(bilangan_1/bilangan_2))
+
+elif calculator =='4':
+    hasil = bilangan_1*bilangan_2
+    print("Result:",(bilangan_1*bilangan_2))
+
+
+
+
+
